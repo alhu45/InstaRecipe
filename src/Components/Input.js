eff3f1c78464d41a6191eff1d0558315f82ae7dd
Alan Hu <17.alanhu@gmail.com>
2024-05-18 03:55:15 -0400

    Upload Button (Placeholder)

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import axios from "axios";
+
+
 function Input() {
+
+    const [ file, setFile ] = useState(null);
+    const [ progress, setProgress ] = useState({ started: false, pc: 0 });
+    const [ msg, setMsg ] = useState(null);
+
+    function handleUpload() {
+        if(!file) {
+            console.log("No File Selected");
+            return;
+        }
+
+        const fd = new FormData();
+        fd.append('file', file);
+
+        axios.post('http:/httpbin.org/post', fd, {
+            onUploadProgress: (progressEvent) => { console.log(progressEvent.progress*100) },
+            headers: {
+                "Custom-Header": "value",
+
+            }
+        })
+        .then(res => console.log(res.data))
+        .catch(err => console.error(err));
+    }
+
     return(
         <>
             <h3><center>Input image of ingredients here</center></h3>
-            <center><input type = "file"/></center>
+            <center>
+                <input onChange = { (e) => { setFile(e.target.files[0]) } } type = "file"/>
+                <button onClick = { handleUpload }>Upload</button>
+            </center>
         </>
     );
 
