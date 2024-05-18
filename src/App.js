6fa928daee2c6c989312a0a15fe439c50d5831a8
Alan Hu <17.alanhu@gmail.com>
2024-05-18 14:09:38 -0400

    Front-end Work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Homepage from "./Components/Homepage.js"
 import Navbar from "./Components/Navbar.js"
 import ImageClassifier from "./Components/ImageClassifier.js";
+import Background from "./Components/Background.js"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Homepage />
+      <Background/>
       <ImageClassifier />
     </>
   );
